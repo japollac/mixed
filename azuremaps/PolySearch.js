a09d414b5be9ac50ce0500aa07f565eeffa64241
Jan Pollack <pollack.jan@gmail.com>
2019-12-09 11:51:53 +0100

    added poly search code snipped

---
+++ azuremaps/PolySearch.js
@@ -0,0 +1,25 @@
+const url1 = "https://atlas.microsoft.com/search/address/json?subscription-key=[sub]&api-version=1.0&query=Niedersachsen&typeahead=true&limit=1"
+            
+fetch(url1).then((response) => {
+               
+    response.json().then((geojson) => {
+    const geometry = geojson.results[0].dataSources.geometry
+
+    let url = "https://atlas.microsoft.com/search/polygon/json?subscription-key=[sub]&api-version=1.0&geometries={geometries}".replace("{geometries}", geometry.id)
+    fetch(url).then((geopoly) => {
+        geopoly.json().then((data) => {
+            const coords = data.additionalData[0].geometryData.features[0].geometry
+            console.log(coords)
+
+            this._dataSource.add(new atlas.data.Feature(
+                new atlas.data.MultiPolygon(coords.coordinates)
+            ))
+
+            map.layers.add(new atlas.layer.PolygonLayer(this._dataSource, null,{
+                fillColor: 'red',
+                opacaty: 0.5
+            }));
+
+        })
+    })
+})
